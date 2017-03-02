6c6ebdc7abf4b4b56d13a7352e05f6fb29c92a50
lewis810k <alsdl901@gmail.com>
2017-03-03 07:18:08 +0900

    ui 수정

--- django_app/slack/urls.py
+++ django_app/slack/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'slack'
 urlpatterns = [
     url(r'^too_fast/$', views.too_fast_view, name='too_fast'),
+    url(r'^selector/$', views.selector_view, name='selector'),
+    url(r'^one_commit/$', views.one_commit_view, name='one_commit'),
 ]
